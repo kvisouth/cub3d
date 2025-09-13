4e18f65f95728115ca866c9a3e05eef435bfff34
nok <visouthivong.kevin@gmail.com>
2025-09-14 01:00:14 +0200

    init

---
+++ inc/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/14 00:56:17 by kevisout          #+#    #+#             */
+/*   Updated: 2025/09/14 00:58:36 by kevisout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <X11/keysym.h>
+# include <pthread.h>
+# include "../libft/libft.h"
+# include "../minilibx-linux/mlx.h"
+# include "X11/X.h"
+
